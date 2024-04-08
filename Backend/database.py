8d3c7d4730e74d156e1152d483b745cb1148bb2d
Jeremy <jeremytk2001@gmail.com>
2024-04-08 14:02:54 -0400

    Deleted mysql connector

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import mysql.connector
 import configparser
 import os.path
 
