0afb1afc4bb63c3f44421a16981582d94d6ed380
Ethan Bielecki <ebielecki2003@gmail.com>
2024-04-16 12:29:26 -0400

    Added dashboard styling

--- Frontend/Components/Navbar.jsx
+++ Frontend/Components/Navbar.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import '../src/CSS/custom_nav_padding.css'
 
 export const Navbar = () => {
--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -1,10 +1,31 @@
 import { Navbar } from "../../Components/Navbar";
+import '../CSS/dashboard_styling.css'
 
 export const Dashboard = () => {
     return (
         <>
             <Navbar/>
-            <h1>Dashboard!</h1>
+            <div className="container_edited">
+                <div className="row">
+                    <div className="dashboard_long_facts dashboard_long_fact_1">
+                        <h1>Fact 1!</h1>
+                    </div>
+                    
+                    <div className="column">
+                        <div className="dashboard_short_facts">
+                            <h1>Fact 2!</h1>
+                        </div>
+
+                        <div className="dashboard_short_facts">
+                            <h1>Fact 3!</h1>
+                        </div>
+                    </div>
+
+                    <div className="dashboard_long_facts dashboard_long_fact_4">
+                        <h1>Fact 4!</h1>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
