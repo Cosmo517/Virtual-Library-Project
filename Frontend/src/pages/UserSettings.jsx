33124e5bc5da132932efc0d998779fd26cc73962
Ethan Bielecki <ebielecki2003@gmail.com>
2024-04-15 21:55:15 -0400

    Added working logout button to user settings page

--- Frontend/src/pages/UserSettings.jsx
+++ Frontend/src/pages/UserSettings.jsx
@@ -1,10 +1,20 @@
 import { Navbar } from "../../Components/Navbar";
+import Cookies from "js-cookie";
 
 export const UserSettings = () => {
+
+    const logout = () => {
+        Cookies.remove('token')
+        window.location.href='/'
+    }
+    
     return (
         <>
             <Navbar/>
-            <h1>User Settings!!</h1>
+            <div className="container">
+                <h3>User Settings:</h3>
+                <button className="btn btn-primary" onClick={logout}>Logout</button>
+            </div>
         </>
     );
 }
