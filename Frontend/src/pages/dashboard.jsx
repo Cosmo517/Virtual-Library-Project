2e389e09a3700abbfd432a0e73e3441f87ac2ebf
Hannah Friday <111408719+hfriday248@users.noreply.github.com>
2024-04-16 14:27:26 -0400

    edited the dashboard
    just wanted to test and edit something easy, will make it better later

--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -7,22 +7,20 @@ export const Dashboard = () => {
             <Navbar/>
             <div className="container_edited">
                 <div className="row">
-                    <div className="dashboard_long_facts dashboard_long_fact_1">
-                        <h1>Fact 1!</h1>
+                    <div className="dashboard_title">
+                        <h1>Welcome to your own Virtual Bookshelf</h1>
+                        <h2>Web portal for the home library</h2>
                     </div>
                     
-                    <div className="column">
-                        <div className="dashboard_short_facts">
-                            <h1>Fact 2!</h1>
-                        </div>
-
-                        <div className="dashboard_short_facts">
-                            <h1>Fact 3!</h1>
-                        </div>
-                    </div>
-
-                    <div className="dashboard_long_facts dashboard_long_fact_4">
-                        <h1>Fact 4!</h1>
+                        <div className="dashboard_description">
+                            <p>
+                                Welcome to your Virtual Booksheld, where you can explore and manage your personal library collection.
+                                This portal is designed to help you browse through a collection of books and discover new ones.
+                                Browse through the collection, search for specfic titles or authors, and even rate your favorite reads.
+                            </p>
+                            <p>
+                                Whether you're an avid reader or just starting, Virtual Bookshelf is here to enhance your experience!
+                            </p>
                     </div>
                 </div>
             </div>
