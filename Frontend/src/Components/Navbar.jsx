78f4e72a975c1effb130ef8aae90fce541549517
Hannah Friday <111408719+hfriday248@users.noreply.github.com>
2024-04-20 21:08:36 -0400

    styling

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -14,38 +14,22 @@ export const Navbar = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
                         <li className="nav-item active">
                             <a className='nav-link' href='#'>
                                 Dashboard
                             </a>
                         </li>
                     </ul>
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav">
                         <li className="nav-item active">
                             <a className='nav-link' href='#/Browse'>
                                 Browse
                             </a>
                         </li>
                     </ul>
-
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <a className='nav-link' href='#/AddingBooks'>
-                                        Add Books
-                                    </a>
-                                </li>
-                            </ul>
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item active">
-                                    <a className='nav-link' href="#/RemovingBooks">
-                                        Remove Books
-                                    </a>
-                                </li>
-                            </ul>
-                    
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav">
                         <li className="nav-item active dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -53,6 +37,8 @@ export const Navbar = () => {
                             </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                 <a className="dropdown-item" href="#/UserSettings">Settings</a>
+                                <a className="dropdown-item" href="#/AddingBooks">Add Books</a>
+                                <a className="dropdown-item" href="#/RemoveBooks">Remove Books</a>
                             </div>
                         </li>
                     </ul>
