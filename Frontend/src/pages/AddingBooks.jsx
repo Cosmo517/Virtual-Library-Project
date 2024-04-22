47f56f2a799d445a899a6f2350bce0a90090aefa
Ethan Bielecki <ebielecki2003@gmail.com>
2024-04-21 20:38:20 -0400

    Quick formatting fixes

--- Frontend/src/pages/AddingBooks.jsx
+++ Frontend/src/pages/AddingBooks.jsx
@@ -65,6 +65,7 @@ export const AddingBooks = ({ isAuthenticated}) => {
         <div className="page-wrapper"> 
             <Navbar isAuthenticated={isAuthenticated }/>
             <div className='container1'>
+                <h5 className='center-text' style={{color: 'white'}}>Enter the information of a book you want to add</h5>
                 <form onSubmit={handleFormSubmit}>
                     <div className='mb-1 mt-3'>
                         <input type='text' className='form-control' placeholder='ISBN' id='isbn' name='isbn' onChange={handleInputChange} value={formData.isbn}/>
